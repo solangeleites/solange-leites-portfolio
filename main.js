5acaf5bebc1aa931254a4b72c355c208fd3f8b5f
solange <solangeleitesg@gmail.com>
2023-01-09 16:49:44 -0300

    realicé mejoras agregando una api del clima

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const $showMoreButtons = document.getElementsByClassName('show-more');
 const $descriptions = document.getElementsByClassName('description');
-const $hour = document.getElementsByClassName('hora');
-const $fecha = document.getElementsByClassName('fecha');
-const $name = document.getElementById('name');
 const $clima = document.getElementById('clima');
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,%20Argentina&appid=9a0d87730cc4cbadcd498be668e87335')
+
   .then(response => response.json())
   .then(data => {
     const temperature = data.main.temp - 273.15;
     const name = data.name;
-    console.log(`La temperatura actual en ${name} es de ${convertCelcius(temperature)}°.`);  });
+    const icon = data.weather.icon;
+    $clima.innerHTML = `La temperatura actual en ${name} es de ${convertCelcius(temperature)}°.`;
+  });
+
 
   const convertCelcius = kelvin => {
     let celsius = Math.round(kelvin - 0);
@@ -34,25 +35,6 @@ const showMoreAndLess = (event, index) => {
     $description.style.display = 'none';
   }
 };
-
-
-//* Funciones para la hora, la fecha
-
-// const updateClock = () => {
-//   const currentTime = new Date();
-//   const hours = currentTime.getHours();
-//   const minutes = currentTime.getMinutes();
-//   const seconds = currentTime.getSeconds();
-
-//   if (minutes < 10) {
-//     minutes = '0' + minutes;
-//   }
-//   if (seconds < 10) {
-//     seconds = '0' + seconds;
-//   }
-//   $hour.textContent = `${hours}:${minutes}:${seconds}`;
-// };
-
 
 
 
